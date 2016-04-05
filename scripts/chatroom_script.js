1c6cc1935a9c1a89e299a445d5ccd60290cfb787
BigYuns <sungyun.chung94@gmail.com>
2016-04-05 03:31:42 -0400

    first commit

---
+++ scripts/chatroom_script.js
@@ -0,0 +1,122 @@
+var socket = io.connect();
+window.addEventListener('load', function(){
+	var hidden = document.getElementById('nicknameField'); 
+	//var input_nickname = window.prompt("What is your nickname?");
+	//hidden.value = input_nickname;
+
+  var messageForm = document.getElementById('messageForm');
+  //messageForm.addEventListener('submit', sendMessage, false);
+  //show the message in five seconds
+  //setInterval(printResult, 3000);
+  
+  // handle incoming messages
+  socket.on('message', function(nickname, message){
+    // display a newly-arrived message 
+    var space = document.getElementById('message'); 
+    space.innerHTML += nickname+ ":  "+ message+ "<br>"; 
+  });
+
+  // handle room membership changes
+  // you may want to consider having separate handlers for members joining, leaving, and changing their nickname
+  /*socket.on('membershipChanged', function(members){
+    // display the new member list
+
+  });*/
+
+  //1.new member join
+  
+  socket.on('newMember', function(nicknames){
+    var user_list = document.getElementById('users'); 
+    user_list.innerHTML = " "; 
+    for(var i =0 ; i<nicknames.length; i++){
+      user_list.innerHTML +=nicknames[i]+"<br>"; 
+    }
+    //user_list.innerHTML +=nicknames[nicknames.length-1]; 
+  });
+
+  //2.a member leave
+  /*socket.on('removeMember', function(nickname){
+
+  }); */
+
+  //3. change their nickname 
+ /* socket.on('nameChange', function(){
+
+  }); */
+
+
+  // get the nickname
+  var nickname = prompt('Enter a nickname:');
+  hidden.value = nickname;
+
+  // join the room
+  socket.emit('join', meta('roomName'), nickname, function(messages){
+    for(var i =0; i<messages.length; i++){
+      var space = document.getElementById('message'); 
+      space.innerHTML += messages[i].nickname+ ":  "+ messages[i].body+ "<br>"; 
+
+    }
+  });
+
+  messageForm.addEventListener('submit', sendMessage, false);
+}, false);
+
+function meta(name) {
+    var tag = document.querySelector('meta[name=' + name + ']');
+    if (tag != null)
+        return tag.content;
+    return '';
+}
+
+function sendMessage(e) {
+  // prevent the page from redirecting
+  e.preventDefault();
+  // get the parameters
+  //var nickname =document.getElementById('nicknameField').value; // get nickname 
+  var message = document.getElementById('messageField').value; // get message 
+  //var post_string = "nickname=" + nickname + "&message=" + message;
+
+  socket.emit('message', message); 
+  // send it to the server
+ /* var req = new XMLHttpRequest();
+  req.open('POST', '/' + meta('roomName') + '/messages', true);
+  req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  req.send(post_string);
+  document.getElementById('messageField').value =""; */
+}
+
+var old_id = -1; 
+var count =0; 
+function printResult() {
+   $.ajax(
+   	{ url: '/'+ meta('roomName')+'/messages.json',
+      context: document.body,
+      success: function(data, textStatus, jqXHR){
+      	var space = document.getElementById('message'); 
+      	if(data.length!=0){
+      		if(count==0){
+      			space.innerHTML += "NICKNAME:" + data[data.length-1].nickname + "<br>"; 
+      			count +=1; 
+      		}
+      		if(data[data.length-1].id!=old_id){
+      			space.innerHTML += data[data.length-1].body + "<br>"; 
+      			old_id = data[data.length-1].id; 
+      		}
+      		console.log(textStatus); 
+      	}
+      	
+      },
+      error : function(jqXHR, textStatus, errorThrown ) {
+      console.log(textStatus);
+      console.log(errorThrown);
+      var err = jQuery.parseJSON(jqXHR.responseText);
+      if(err.error) {
+        console.log(err.msg);
+        if(err.redirect) {
+          document.location.href = "/";
+        }
+        return;
+      	}
+      }
+   });
+}
---
+++ server.js
@@ -0,0 +1,225 @@
+var express = require('express'), 
+	bodyParser = require("body-parser"), 
+	anyDB = require('any-db'),
+	path    = require("path"),
+	engines = require('consolidate'); 
+
+
+
+/* Connection to the databse called chatroom.db and create a table*/
+var conn = anyDB.createConnection('sqlite3://chatroom.db'); 
+/*Q :On the instruction:  Note well that room names must be unique, 
+and you will get a database error if you try to reuse one. 
+Be sure to handle that error, and generate a new identifier under those circumstances.
+A: UNIQUE can be the solution? Or should I handle the situation? */
+var sql_create = 'CREATE TABLE messages (id INTEGER PRIMARY KEY AUTOINCREMENT, room TEXT NOT NULL, nickname TEXT, body TEXT, time INTEGER )'; 
+conn.query(sql_create).on('end', function(){
+	console.log("Made table!"); 
+})
+
+/*Query */
+
+/*Build the app in express*/
+var app = express(); 
+
+var http = require('http'); 
+var server = http.createServer(app); 
+
+// add socket.io
+var io = require('socket.io').listen(server);
+/*tell Express to run .html files through Hogan */
+app.engine('html', engines.hogan); 
+/*tell Express where to find templates*/
+app.set('views', __dirname + '/templates'); 
+app.use("/scripts", express.static(__dirname+'/scripts')); 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var test_set = new Set(); 
+app.get('/', function(req,res){
+	//console.log(__dirname+"/templates/index.html"); 
+ 	res.render('index.html'); 
+});
+
+app.get('/generate', function(request,response){
+	var chatroomId = generateRoomIdentifier(); 
+	while(test_set.has(chatroomId)){
+		chatroomId = generateRoomIdentifier(); 
+	}
+	test_set.add(chatroomId); 
+	response.redirect('/'+chatroomId); 
+}); 
+
+app.get('/:roomName', function(request,response){
+	var name = request.params.roomName; 
+	if(name != 'favicon.ico'){
+		response.render('chatroom.html', {roomName: name}); 
+	}
+}); 
+
+
+//var users = new Set([]);
+//var users=[]; 
+io.sockets.on('connection', function(socket){
+    // clients emit this when they join new rooms
+    socket.on('join', function(roomName, nickname, callback){
+    
+        socket.join(roomName); // this is a socket.io method
+        socket.nickname = nickname; // yay JavaScript! see below
+        // get a list of messages currently in the room, then send it back
+        //users.add(nickname); 
+
+        var clients_in_the_room = io.sockets.in(roomName);
+                
+        var users = [];
+        for (var s in clients_in_the_room.connected){
+            var client_nickname = io.sockets.connected[s].nickname;
+            
+            if(client_nickname!=undefined){
+                console.log(client_nickname);
+                users.push(client_nickname); 
+            } 
+        }
+        /*for (var socket in clients_in_the_room.connected) {
+                  var client_nickname = io.sockets.connected[socket].nickname;
+                  if(client_nickname != undefined) {
+                    console.log(client_nickname);
+                    users.push(client_nickname);
+                  }
+                }*/
+        //users.push(nickname); 
+        //users[nickname] = nickname; 
+        io.sockets.in(roomName).emit('newMember', users); 
+        var messages = [];
+       	var sql = 'SELECT id, nickname, body FROM messages WHERE room='+'"'+roomName+'"';
+        var q = conn.query(sql, function(err,result){
+        	if(result.rows.length!=0){
+        		for(var i=0; i<result.rows.length; i++){
+        			var row = result.rows[i]; 
+        			var item = {
+        				id: row.id, 
+        				nickname: row.nickname,
+        				body: row.body 
+        			}
+        			if(item!=undefined){
+        				messages.push(item);
+        				//users.add(item.nickname); 
+        				//console.log(users); 
+        				
+        			}
+        		}//for loop
+        	}//if statement 
+        	callback(messages);
+        });
+    });
+
+    // this gets emitted if a user changes their nickname
+    socket.on('nickname', function(nickname){
+        socket.nickname = nickname;
+        //broadcast update to room! (see below)
+        //broadcastNickNameChanged(room_name, nickname); 
+    });
+
+    // the client emits this when they want to send a message
+    socket.on('message', function(message){
+        // process an incoming message (don't forget to broadcast it to everyone!)
+
+        // note that you somehow need to determine what room this is in
+        // io.of(namespace).adapter.rooms[socket.id] may be of some help, or you
+        // could consider adding another custom property to the socket object.
+
+        // Note that io.sockets.adapter.sids is a hash mapping
+        // from room name to true for all rooms that the socket is in.
+        // The first member of the list is always the socket itself,
+        // and each successive element is a room the socket is in,
+        // So, to get the room name without adding another custom property,
+        // you could do something like this:
+        
+        var roomName = Object.keys(io.sockets.adapter.sids[socket.id])[1];
+        var nickname = socket.nickname; 
+        var sql_insert = 'INSERT INTO messages VALUES ($1, $2, $3, $4,$5)'; 
+    	conn.query(sql_insert, [,roomName, nickname,message, null]); 
+
+    	// then send the message to users! 
+    	//call the client side event 
+    	io.sockets.in(roomName).emit('message', nickname, message);
+    }); 
+    // the client disconnected/closed their browser window
+    socket.on('disconnect', function(){
+    	console.log("disconnect"); 
+    	console.log(io.sockets.adapter.sids[socket.id]); 
+    	if(io.sockets.adapter.sids[socket.id]!=undefined){
+    		//var roomName = Object.keys(io.sockets.adapter.sids[socket.id])[1];
+            var roomName = socket.roomName; 
+            console.log("")
+    		socket.leave(roomName); 
+    	}
+        
+    });
+});
+
+/*app.post('/:roomName/messages', function(request, response){
+    var name = request.params.roomName;   // 'ABC123'
+    var nickname = request.body.nickname; // 'Trump'
+    var message = request.body.message;   // 'The beauty of me is that Im very rich' 
+    var sql_insert = 'INSERT INTO messages VALUES ($1, $2, $3, $4,$5)'; 
+    conn.query(sql_insert, [,name, nickname,message, null]); 
+    response.redirect('/' + request.params.roomName);
+}); */
+
+/*var messages = [];
+var old_id = -1; 
+app.get('/:roomName/messages.json', function(request, response){
+    //{nickname: 'Trump', body: 'Good people don't go into politics!'}, ...
+    var room_name = request.params.roomName; 
+	var sql = 'SELECT id, nickname, body FROM messages WHERE room='+'"'+room_name+'"';
+	var q = conn.query(sql, function(err,result){
+		if(result.rows.length!=0){
+			var row = result.rows[result.rows.length-1]; 
+			var item = {
+			id: row.id, 
+			nickname: row.nickname,
+			body: row.body 
+			}
+		}
+		if((item!=undefined) && (old_id!=item.id)){
+			messages.push(item); 
+			old_id = item.id; 
+		}
+		response.json(messages);
+	});
+});*/
+
+
+app.get('*', function(req,res){
+	//console.log(__dirname+"/templates/index.html"); 
+ 	res.sendFile(path.join(__dirname+'/templates/index.html'));
+});
+
+/* List to the port 8080*/
+/*app.listen(8080,function(){
+	console.log("-Server Listening on port 8080"); 
+
+}); */
+server.listen(8080); 
+
+function generateRoomIdentifier() {
+   var chars = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
+   var result = '';
+   for (var i = 0; i < 6; i++)
+   	result += chars.charAt(Math.floor(Math.random() * chars.length));
+   return result;
+}
+
+
+/* broadcast changed information*/
+function broadcastNickNameChanged(roomName, nickname){
+	// send them out
+	//nameChange should be defined in the client side 
+    io.sockets.in(roomName).emit('nameChange', nickname);
+}
+
+function broadcastMemberJoined(roomName, nickname) {
+    // send them out
+    io.sockets.in(roomName).emit('newMember', nickname);
+}
